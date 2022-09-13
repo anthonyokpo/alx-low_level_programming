b7ce3cb4873706dca8fbe6ca4dfa7659f04e4fb4
anthonyokpo <okpoanthony1@gmail.com>
2022-09-13 14:00:15 -0700

    Print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
 */
 void print_alphabet(void)
 {
-	char c;
+	char a = 'a';
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (a = 'a'; a <= 'z'; a++)
 	{
-		_putchar(c);
+		_putchar(a);
 	}
 	_putchar('\n');
 }
