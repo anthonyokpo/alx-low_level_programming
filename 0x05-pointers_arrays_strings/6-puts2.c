83e61c051e0df6bc32b14d8c682a8384a9c0967c
anthonyokpo <okpoanthony1@gmail.com>
2022-09-20 03:09:34 -0700

    Puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code.
+ * @str: pointer declared.
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
