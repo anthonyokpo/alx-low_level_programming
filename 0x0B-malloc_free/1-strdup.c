0872502f3829edceb6c543d52e3709553499a3a1
anthonyokpo <okpoanthony1@gmail.com>
2022-10-10 03:40:58 -0700

    Returns pointer to allocated space in the memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - check the code for ALX School students.
+ * @str: pointer
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
+}
