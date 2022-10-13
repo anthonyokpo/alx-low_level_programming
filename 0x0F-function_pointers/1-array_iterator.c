226d66cad2a94cd009b1f77aac21fad0789c3a0b
anthonyokpo <okpoanthony1@gmail.com>
2022-10-13 03:42:56 -0700

    Fuction that executes a fxn

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function given, each el of the array
+ * @array: the array
+ * @size: the size of the array
+ * @action: the function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
