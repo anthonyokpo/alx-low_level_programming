bd3e8b7229cc4c5d8ef37d2eb871723177719456
anthonyokpo <okpoanthony1@gmail.com>
2022-10-13 03:44:56 -0700

    Fuction that executes a fxn

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,12 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array != NULL && action != NULL && size > 0)
+	if (array != NULL && action != NULL)
 	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
+	for (; i < size; i++)
+		action(array[i]);
 	}
 }
